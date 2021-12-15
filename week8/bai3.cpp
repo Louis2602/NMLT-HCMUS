30bf632b092ccf1602c764b1a39cfa51ba127b67
Louis2602 <trantunglam135790@gmail.com>
2021-12-15 21:17:16 +0700

    add new files

--- week8/bai3.cpp
+++ week8/bai3.cpp
@@ -8,28 +8,25 @@ int countAverageChar(char input[])
     fin.open(input);
     char ch;
     int cnt = 0;
-    int avg = 0;
     int line = 0;
-    fin.get(ch);
-    while (fin)
+    while (fin.get(ch))
     {
-        while (fin && ch != '\n')
+        if (ch != '\n')
         {
             cnt++;
-            avg += int(ch);
-            fin.get(ch);
         }
-        line++;
-        cout << "gia tri trung binh dong " << line << ": " << avg / cnt << endl;
-        avg = 0;
-        cnt = 0;
-        fin.get(ch);
+        else
+        {
+            line++;
+        }
     }
+    cout << cnt << " " << line << endl;
+    return cnt / line;
     fin.close();
 }
 int main()
 {
     char input[] = "input.txt";
-    countAverageChar(input);
+    cout << "Gia tri trung binh so luong ki tu tren 1 dong la: " << countAverageChar(input);
     return 0;
 }
