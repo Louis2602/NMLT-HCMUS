b285766b7a32c33ee06a638a714e8919387a97b8
Louis2602 <trantunglam135790@gmail.com>
2021-12-28 23:43:49 +0700

    updates

--- exercise/dayConTongBangS.cpp
+++ exercise/dayConTongBangS.cpp
@@ -3,18 +3,10 @@ using namespace std;
 
 int main()
 {
-    int n, s = 6;
-    int a[] = {1, 2, 3, 4, 5}, dp[40005] = {0};
+    int n, s = 3;
+    int a[] = {1, 2, 3, 4, 5}, dp[40005];
     n = sizeof(a) / sizeof(int);
     dp[0] = 1;
-    int k = 0;
-    for (int i = 0; i < n; ++i)
-        k += a[i];
-    if (s > k)
-    {
-        cout << "NO";
-        return 0;
-    }
     for (int i = 1; i <= n; ++i)
         for (int j = s; j >= a[i]; --j)
             if (dp[j - a[i]] == 1)
