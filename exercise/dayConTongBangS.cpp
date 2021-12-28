e6d1b647c4eaf21806b22a959ba6c45652b8e9a4
Louis2602 <trantunglam135790@gmail.com>
2021-12-28 09:46:07 +0700

    add new files

---
+++ exercise/dayConTongBangS.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, s = 16;
+    int a[] = {1, 2, 3, 4, 5}, dp[40005] = {0};
+    n = sizeof(a) / sizeof(int);
+    dp[0] = 1;
+    int k = 0;
+    for (int i = 0; i < n; ++i)
+        k += a[i];
+    if (s > k)
+    {
+        cout << "NO";
+        return 0;
+    }
+    for (int i = 1; i <= n; ++i)
+        for (int j = s; j >= a[i]; --j)
+            if (dp[j - a[i]] == 1)
+                dp[j] = 1;
+    if (dp[s] == 1)
+        cout << "YES";
+    else
+        cout << "NO";
+    return 0;
+}
